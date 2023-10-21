e2ffcd8715ff45c5e81f0d460ed8dbfc7a57cad7
AnnaLew <57362758+AnnaLew@users.noreply.github.com>
2023-10-21 11:31:18 +0200

    Added description

--- code/Bash-code/sequences-script.sh
+++ code/Bash-code/sequences-script.sh
@@ -1,1 +1,3 @@
+# This script downloads the genomes, takes the list of links to the database as input
+
 while read line; do  wget -qN $(echo $line| tr -d '\"') -P ./bartonella-50sampleData ; done < barto50.txt
