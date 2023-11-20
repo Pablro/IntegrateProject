25115a2b1b0703012d4edd9047f8dfdaeca202d9
Pablro <92064080+Pablro@users.noreply.github.com>
2023-11-20 23:45:03 +0100

    update to nesse2 for pangenome size
    I added the analysis for yanlin dataset

--- code/R_code/PangenomeSizeAnalysis.R
+++ code/R_code/PangenomeSizeAnalysis.R
@@ -1,3 +1,4 @@
+library("readr")
 library("micropan")
 # Based on: https://github.com/larssnip/micropan
 library("collapse")
@@ -18,11 +19,11 @@ load("50Nesse1panmatrixCDS.RData")
 #sample of 400
 load("400Nesse1panmatrixCDS.RData")
 
-#Loading ncRNA panmatrix data
+#Neisseria sample 2
 #sample 50
-load("50Nesse1panmatrixncRNA.RData")
+load("50Nesse2panmatrixCDS.RData")
 #sample 400
-load("400Nesse1panmatrixncRNA.RData")
+load("400Nesse2panmatrixCDS.RData")
 
 #Pangenome size analysis
 #Neisseria sample 1
@@ -34,17 +35,36 @@ LargePanMatNesseOne<-as.matrix(LargePanMatNesseOne)
 smallPanMatNesseOne<-replace_NA(smallPanMatNesseOne)
 LargePanMatNesseOne<-replace_NA(LargePanMatNesseOne)
 
+#Neisseria sample 2
+smallPanMatNesseTwo<-as.matrix(smallPanMatNesseTwo)
+LargePanMatNesseTwo<-as.matrix(LargePanMatNesseTwo)
+#Replace NA with zeros
+smallPanMatNesseTwo<-replace_NA(smallPanMatNesseTwo)
+LargePanMatNesseTwo<-replace_NA(LargePanMatNesseTwo)
+
 #Heaps Law:
+#neisseria 1
 smallheapsnesse=heaps(smallPanMatNesseOne)
 largeheapsnesse=heaps(LargePanMatNesseOne)
+#Neisseria 2
+smallheapsnesse2=heaps(smallPanMatNesseTwo)
+largeheapsnesse2=heaps(LargePanMatNesseTwo)
+
 #if alpha is greater than 1 is closed otherwise is open
 smallheapsnesse
-largeheapsnesse
+smallheapsnesse2
 # Frequencies estimation by fitting binomial mixture model
+#Neisseria 1
 fittedsmall <- binomixEstimate(smallPanMatNesseOne, K.range = 3:20)
 fittedlarge<- binomixEstimate(LargePanMatNesseOne, K.range = 3:20)
 print(fittedsmall$BIC.tbl)
 print(fittedlarge$BIC.tbl)
+#Neisseria sample 2
+View(smallPanMatNesseTwo)
+fittedsmall2<- binomixEstimate(smallPanMatNesseTwo, K.range = 3:20)
+fittedlarge2<- binomixEstimate(LargePanMatNesseTwo, K.range = 3:20)
+print(fittedsmall2$BIC.tbl)
+print(fittedlarge2$BIC.tbl)
 #Small Dataset
 ncomp <- 13
 fig4 <- fittedsmall$Mix.tbl %>% 
@@ -52,11 +72,13 @@ fig4 <- fittedsmall$Mix.tbl %>%
   ggplot() +
   geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
   coord_polar(theta = "y") +
-  labs(x = "", y = "", title = "Pan-genome gene family distribution",
-       fill = "Detection\nprobability")
+  labs(x = "", y = "", title = "Pan-genome gene family distribution")+ 
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
 #Core genomes are presume to be in the fraction with a detection probability of 1
-#for the rest nothing can be conclude
+#for the rest read the paper of micropan.
 print(fig4)
+
+
 #Large Dataset
 ncomp<-16
 fig5<-fittedlarge$Mix.tbl %>% 
@@ -64,57 +86,37 @@ fig5<-fittedlarge$Mix.tbl %>%
   ggplot() +
   geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
   coord_polar(theta = "y") +
-  labs(x = "", y = "", title = "Pan-genome gene family distribution",
-       fill = "Detection\nprobability")
+  labs(x = "", y = "", title = "Pan-genome gene family distribution") +
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
 #Core genomes are presume to be in the fraction with a detection probability of 1
-#for the rest nothing can be conclude
+#for the rest read the paper of micropan.
 print(fig5)
-# ncRNA pangenome
-#Convert to matrix
-smallncRNAPanMatNesseOne<-as.matrix(smallncRNAPanMatNesseOne)
-LargencRNAPanMatNesseOne<-as.matrix(LargencRNAPanMatNesseOne)
-#Replace NA with zeros
-smallncRNAPanMatNesseOne<-replace_NA(smallncRNAPanMatNesseOne)
-LargencRNAPanMatNesseOne<-replace_NA(LargencRNAPanMatNesseOne)
-
-#Heaps Law:
-smallheapsnessencRNA=heaps(smallncRNAPanMatNesseOne)
-largeheapsnessencRNA=heaps(LargencRNAPanMatNesseOne)
-#if alpha is greater than 1 is closed otherwise  is open
-smallheapsnessencRNA
-largeheapsnessencRNA
-# Frequencies estimation by fitting binomial mixture model
-fittedsmallncRNA <- binomixEstimate(smallncRNAPanMatNesseOne, K.range = 3:20)
-fittedlargencRNA<- binomixEstimate(LargencRNAPanMatNesseOne, K.range = 3:20)
-print(fittedsmallncRNA$BIC.tbl)
-print(fittedlargencRNA$BIC.tbl)
-#Small Dataset
-ncomp <- 4
-fig6 <- fittedsmallncRNA$Mix.tbl %>% 
-  filter(Components == ncomp) %>% 
-  ggplot() +
-  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
-  coord_polar(theta = "y") +
-  labs(x = "", y = "", title = "Pan-genome gene family distribution",
-       fill = "Detection\nprobability")
-#Core genomes are presume to be in the fraction with a detection probability of 1
-#for the rest nothing can be conclude
-print(fig6)
-#Large Dataset
-ncomp<-8
-fig7<-fittedlargencRNA$Mix.tbl %>% 
-  filter(Components == ncomp) %>% 
-  ggplot() +
-  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
-  coord_polar(theta = "y") +
-  labs(x = "", y = "", title = "Pan-genome gene family distribution",
-       fill = "Detection\nprobability")
-#Core genomes are presume to be in the fraction with a detection probability of 1
-#for the rest nothing can be conclude
-print(fig7)
 
 #Neisseria sample 2
-
+#Small Dataset
+ncomp <- 8
+fig6 <- fittedsmall2$Mix.tbl %>% 
+  filter(Components == ncomp) %>% 
+  ggplot() +
+  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
+  coord_polar(theta = "y") +
+  labs(x = "", y = "", title = "Pan-genome gene family distribution")+ 
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
+#Core genomes are presume to be in the fraction with a detection probability of 1
+#for the accesory genomes. read the paper of micropan.
+print(fig6)
+#Large dataset
+ncomp<-15
+fig7<-fittedlarge2$Mix.tbl %>% 
+  filter(Components == ncomp) %>% 
+  ggplot() +
+  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
+  coord_polar(theta = "y") +
+  labs(x = "", y = "", title = "Pan-genome gene family distribution") +
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
+#Core genomes are presume to be in the fraction with a detection probability of 1
+#for the rest read the paper of micropan.
+print(fig7)
 #Bacteroides sample 1
 
 #Bacteroides sample 2
