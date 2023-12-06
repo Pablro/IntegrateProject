0c4869a6f50aaec382713bede2a27db72cb37549
Pablro <92064080+Pablro@users.noreply.github.com>
2023-12-06 02:35:00 +0100

    modification to the micropan R file
    Includes all samples

--- code/R_code/PangenomeSizeAnalysis.R
+++ code/R_code/PangenomeSizeAnalysis.R
@@ -1,4 +1,3 @@
-library("readr")
 library("micropan")
 # Based on: https://github.com/larssnip/micropan
 library("collapse")
@@ -24,7 +23,16 @@ load("400Nesse1panmatrixCDS.RData")
 load("50Nesse2panmatrixCDS.RData")
 #sample 400
 load("400Nesse2panmatrixCDS.RData")
-
+#Bacteroides sample 1
+#sample 50
+load("50Bactero1panmatrixCDS.RData")
+#sample 400
+load("400Bactero1panmatrixCDS.RData")
+#Bacteroides sample 2
+#sample 50
+load("50Bactero2panmatrixCDS.RData")
+#sample 400
+load("400Bactero2panmatrixCDS.RData")
 #Pangenome size analysis
 #Neisseria sample 1
 #CDS pangenome
@@ -41,33 +49,66 @@ LargePanMatNesseTwo<-as.matrix(LargePanMatNesseTwo)
 #Replace NA with zeros
 smallPanMatNesseTwo<-replace_NA(smallPanMatNesseTwo)
 LargePanMatNesseTwo<-replace_NA(LargePanMatNesseTwo)
-
+#Bacteroides sample 1
+smallPanMatBacteroOne<-as.matrix(smallPanMatBacteroOne)
+LargePanMatBacteroOne<-as.matrix(LargePanMatBacteroOne)
+#Replace NA with zeros
+smallPanMatBacteroOne<-replace_NA(smallPanMatBacteroOne)
+LargePanMatBacteroOne<-replace_NA(LargePanMatBacteroOne)
+#Bacteroides sample 2
+smallPanMatBacteroTwo<-as.matrix(smallPanMatBacteroTwo)
+LargePanMatBacteroTwo<-as.matrix(LargePanMatBacteroTwo)
+#Replace NA with zeros
+smallPanMatBacteroTwo<-replace_NA(smallPanMatBacteroTwo)
+LargePanMatBacteroTwo<-replace_NA(LargePanMatBacteroTwo)
 #Heaps Law:
 #neisseria 1
 smallheapsnesse=heaps(smallPanMatNesseOne)
 largeheapsnesse=heaps(LargePanMatNesseOne)
+smallheapsnesse
+largeheapsnesse
 #Neisseria 2
 smallheapsnesse2=heaps(smallPanMatNesseTwo)
 largeheapsnesse2=heaps(LargePanMatNesseTwo)
-
-#if alpha is greater than 1 is closed otherwise is open
-smallheapsnesse
 smallheapsnesse2
+largeheapsnesse2
+#Bacteroides 1
+smallheapsbactero=heaps(smallPanMatBacteroOne)
+largeheapsbactero=heaps(LargePanMatBacteroOne)
+smallheapsbactero
+largeheapsbactero
+#Bacteroides 2
+smallheapsbactero2=heaps(smallPanMatBacteroTwo)
+largeheapsbactero2=heaps(LargePanMatBacteroTwo)
+smallheapsbactero2
+largeheapsbactero2
+#if alpha is greater than 1 is closed otherwise is open
 # Frequencies estimation by fitting binomial mixture model
 #Neisseria 1
-fittedsmall <- binomixEstimate(smallPanMatNesseOne, K.range = 3:20)
-fittedlarge<- binomixEstimate(LargePanMatNesseOne, K.range = 3:20)
-print(fittedsmall$BIC.tbl)
-print(fittedlarge$BIC.tbl)
+nessefittedsmall <- binomixEstimate(smallPanMatNesseOne, K.range = 3:20)
+nessefittedlarge<- binomixEstimate(LargePanMatNesseOne, K.range = 3:20)
+print(nessefittedsmall$BIC.tbl)
+print(nessefittedlarge$BIC.tbl)
 #Neisseria sample 2
-View(smallPanMatNesseTwo)
-fittedsmall2<- binomixEstimate(smallPanMatNesseTwo, K.range = 3:20)
-fittedlarge2<- binomixEstimate(LargePanMatNesseTwo, K.range = 3:20)
-print(fittedsmall2$BIC.tbl)
-print(fittedlarge2$BIC.tbl)
+nessefittedsmall2<- binomixEstimate(smallPanMatNesseTwo, K.range = 3:20)
+nessefittedlarge2<- binomixEstimate(LargePanMatNesseTwo, K.range = 3:20)
+print(nessefittedsmall2$BIC.tbl)
+print(nessefittedlarge2$BIC.tbl)
+#Bacteroides sample 1
+bacterofittedsmall <- binomixEstimate(smallPanMatBacteroOne, K.range = 3:20)
+bacterofittedlarge<- binomixEstimate(LargePanMatBacteroOne, K.range = 3:20)
+print(bacterofittedsmall$BIC.tbl)
+print(bacterofittedlarge$BIC.tbl)
+#Bacteroides sample 2
+bacterofittedsmall2<- binomixEstimate(smallPanMatBacteroTwo, K.range = 3:20)
+bacterofittedlarge2<- binomixEstimate(LargePanMatBacteroTwo, K.range = 3:20)
+print(bacterofittedsmall2$BIC.tbl)
+print(bacterofittedlarge2$BIC.tbl)
+
+#Neisseria 1
 #Small Dataset
 ncomp <- 13
-fig4 <- fittedsmall$Mix.tbl %>% 
+fig4 <- nessefittedsmall$Mix.tbl %>% 
   filter(Components == ncomp) %>% 
   ggplot() +
   geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
@@ -81,7 +122,7 @@ print(fig4)
 
 #Large Dataset
 ncomp<-16
-fig5<-fittedlarge$Mix.tbl %>% 
+fig5<-nessefittedlarge$Mix.tbl %>% 
   filter(Components == ncomp) %>% 
   ggplot() +
   geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
@@ -95,7 +136,7 @@ print(fig5)
 #Neisseria sample 2
 #Small Dataset
 ncomp <- 8
-fig6 <- fittedsmall2$Mix.tbl %>% 
+fig6 <- nessefittedsmall2$Mix.tbl %>% 
   filter(Components == ncomp) %>% 
   ggplot() +
   geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
@@ -107,7 +148,7 @@ fig6 <- fittedsmall2$Mix.tbl %>%
 print(fig6)
 #Large dataset
 ncomp<-15
-fig7<-fittedlarge2$Mix.tbl %>% 
+fig7<-nessefittedlarge2$Mix.tbl %>% 
   filter(Components == ncomp) %>% 
   ggplot() +
   geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
@@ -118,5 +159,53 @@ fig7<-fittedlarge2$Mix.tbl %>%
 #for the rest read the paper of micropan.
 print(fig7)
 #Bacteroides sample 1
-
+#Small Dataset
+ncomp <- 11
+fig8 <- bacterofittedsmall$Mix.tbl %>% 
+  filter(Components == ncomp) %>% 
+  ggplot() +
+  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
+  coord_polar(theta = "y") +
+  labs(x = "", y = "", title = "Pan-genome gene family distribution")+ 
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
+#Core genomes are presume to be in the fraction with a detection probability of 1
+#for the accesory genomes. read the paper of micropan.
+print(fig8)
+#Large dataset
+ncomp<-21
+fig9<-bacterofittedlarge$Mix.tbl %>% 
+  filter(Components == ncomp) %>% 
+  ggplot() +
+  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
+  coord_polar(theta = "y") +
+  labs(x = "", y = "", title = "Pan-genome gene family distribution") +
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
+#Core genomes are presume to be in the fraction with a detection probability of 1
+#for the rest read the paper of micropan.
+print(fig9)
+#Error in this plot
 #Bacteroides sample 2
+#Small Dataset
+ncomp <- 8
+fig10 <- bacterofittedsmall2$Mix.tbl %>% 
+  filter(Components == ncomp) %>% 
+  ggplot() +
+  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
+  coord_polar(theta = "y") +
+  labs(x = "", y = "", title = "Pan-genome gene family distribution")+ 
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
+#Core genomes are presume to be in the fraction with a detection probability of 1
+#for the accesory genomes. read the paper of micropan.
+print(fig10)
+#Large dataset
+ncomp<-13
+fig11<-bacterofittedlarge2$Mix.tbl %>% 
+  filter(Components == ncomp) %>% 
+  ggplot() +
+  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
+  coord_polar(theta = "y") +
+  labs(x = "", y = "", title = "Pan-genome gene family distribution") +
+  scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
+#Core genomes are presume to be in the fraction with a detection probability of 1
+#for the rest read the paper of micropan.
+print(fig11)
